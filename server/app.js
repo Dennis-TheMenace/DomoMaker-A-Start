f54f0c93b53fb77c35d72e17fc1518e6d2ac3bdd
Alex Gray <arg2399@rit.edu>
2022-03-24 12:55:33 -0400

    fixed error

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://ocalhost/Domomaker';
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/Domomaker';
 mongoose.connect(dbURI, (err) =>
 {
     if(err)
